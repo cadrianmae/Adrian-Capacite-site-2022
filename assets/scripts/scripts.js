e3a40af855139c00108cd8606022a662a0c510c7
Adrian Capacite <capacite.adrian.thomas@gmail.com>
2021-12-16 21:15:40 +0000

    implemented contact form

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,12 +1,10 @@
-// Initialisation
-window.onload = function() {
-
-};
-
-// NAV FUNCTIONS
+// NAV FUNCTIONS - Functionality 1
 // Toggle mobile nav
 function toggleNav() {
     document.getElementById("primary-nav").classList.toggle("nav-open");
+}
+function closeNav() {
+    document.getElementById("primary-nav").classList.remove("nav-open");
 }
 // Hide desktop nav on scroll down
 var lastScrollY = window.pageYOffset;
@@ -24,11 +22,7 @@ window.onscroll = function() {
 
 // END OF NAV FUNCTIONS
 
-// CAROUSEL FUNCTIONS
-function carouselInitAll() {
-    
-}
-
+// CAROUSEL FUNCTIONS - Functionality 2
 // Returns elements and values of the carousel
 function carouselGetProperties(elem) {
     let wrapperElem = elem.parentElement.parentElement;
@@ -103,19 +97,114 @@ function carouselUpdate(elem) {
 }
 // END OF CAROUSEL FUNCTIONS
 
-// FORM VALIDATION
+// FORM VALIDATION - Functionality 3
 // Open modal contacts form
-function toggleContactModal() {
+function openContactModal() {
     let elem = document.getElementById("contact-modal");
     let body = document.getElementsByTagName("body")[0];
-    elem.classList.toggle("modal-open");
-    if (elem.classList.contains("modal-open")) {
-        body.classList.add("no-scroll");
-    } else {
-        
-        body.classList.remove("no-scroll");
-    }
+    elem.classList.add("modal-open");
+    body.classList.add("no-scroll");
+    closeNav()
 }
-function validateForm() {
+
+function closeContactModal() {
+    let elem = document.getElementById("contact-modal");
+    let body = document.getElementsByTagName("body")[0];
+    elem.classList.remove("modal-open");
+    body.classList.remove("no-scroll");
+
+}
+
+// Form validation
+// Regex constants
+const patternEmpty = /^(?![\S])$/;
+const patternName = /^[^<>()\[\]\\/.,;:*\s@]+(\s{1}[^<>()\[\]\\/.,;:*\s@]+)*$/;
+const patternEmail = /^([^<>()\[\]\\.,;:\s@]+(\.+[^<>()\[\]\\.,;:\s@]+)*)@(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})$/;
+
+function validateForm(formElem) {
+    const formName = formElem.attributes.getNamedItem("name").value;
+    const outputElem = formElem.getElementsByClassName("submit-output")[0];
+    let isValid = true;
+
+    clearFormErrors(formName);
+
+    if (validateField(formName, "name", "name") == false) {
+        isValid = false;
+    }
+    if (validateField(formName, "email", "email") == false) {
+        isValid = false;
+    }
+
+    if (validateRadioCheckboxes(formName, "reason") == false) {
+        isValid = false;
+    }
+    if (validateRadioCheckboxes(formName, "subject") == false) {
+        isValid = false;
+    }
+
+    
+    if (validateField(formName, "visitor-origin") == false) {
+        isValid = false;
+    }
+    if (validateField(formName, "message") == false) {
+        isValid = false;
+    }
+
+    
+    outputElem.classList.remove("success", "error");
+    if (isValid == true) {
+        console.log("validated");
+        outputElem.classList.add("success");
+        outputElem.innerHTML = "Form submitted successfully";
+    } else {
+        console.log("invalid form")
+        outputElem.classList.add("error");
+        outputElem.innerHTML = "Invalid form fields";
+    }
+
+    return isValid;
+}
+function validateField(formName = "", fieldName = "", format = "") {
+    const field = document.forms[formName][fieldName];
+    const fieldWrapper = field.parentElement;
+    const value = field.value;
+    const required = fieldWrapper.classList.contains("required");
+
+    if (required == true && patternEmpty.test(value) == true) {
+        fieldWrapper.classList.add("field-empty");
+        return false;
+    }
+    if (format == "name" && patternName.test(value) == false) {
+        fieldWrapper.classList.add("field-invalid-chars");
+        return false;
+    }
+    if (format == "email" && patternEmail.test(value) == false) {
+        fieldWrapper.classList.add("field-invalid-email");
+        return false;
+    }
+
+    return true;
+}
+function validateRadioCheckboxes(formName, fieldName) {
+    const fieldOptions = document.forms[formName][fieldName];
+    const fieldWrapper = fieldOptions[0].parentElement.parentElement;
+    const required = fieldWrapper.classList.contains("required");
+
+    if (required == true) {
+        for (let i = 0; i < fieldOptions.length; i++) {
+            if (fieldOptions[i].checked) {
+                return true;
+            }
+        }
+    }
+
+    fieldWrapper.classList.add("field-empty");
     return false;
+}
+function clearFormErrors(formName) {
+    const formInputWrappers = document.forms[formName].getElementsByClassName("input");
+
+    for (let i = 0; i < formInputWrappers.length; i++) {
+        formInputWrappers[i].classList.remove("field-empty", "field-invalid-chars", "field-invalid-email");
+    }
 }
